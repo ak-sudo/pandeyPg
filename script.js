58fe359d0af7e935c6f2f0568adf9c66d1bc8d68
ak-sudo <72704561+ak-sudo@users.noreply.github.com>
2025-03-03 12:33:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+let carousel = document.querySelectorAll('.slideFeature-content');
+console.log(carousel.length);
+
+let rightBtn = document.querySelector('#rightBtn');
+let lefttBtn = document.querySelector('#leftBtn');
+
+i=1
+
+carousel[i-1].setAttribute('class', 'slideFeature-content active');
+
+
+rightBtn.addEventListener('click', () => {
+    if (i<carousel.length){
+        carousel[i-1].setAttribute('class', 'slideFeature-content inactive');
+        carousel[i].setAttribute('class', 'slideFeature-content active');
+        i++;
+    }
+    else{
+        carousel[carousel.length-1].setAttribute('class', 'slideFeature-content inactive');
+        carousel[0].setAttribute('class', 'slideFeature-content active');
+        i=1;
+    }
+})
+
+leftBtn.addEventListener('click', () => {
+
+    getActive = document.querySelector('.active');
+    getActiveId = getActive['id']-1;
+
+    if (getActiveId<carousel.length){
+        if (getActiveId=0){
+            carousel[carousel.length-i].setAttribute('class', 'slideFeature-content active');
+            carousel[i-1].setAttribute('class', 'slideFeature-content inactive');
+            i=carousel.length-1;
+            
+        }
+        else{
+            carousel[getActiveId].setAttribute('class', 'slideFeature-content inactive');
+            carousel[getActiveId-1].setAttribute('class', 'slideFeature-content active');
+
+
+        }
+        console.log(i);
+
+    }
+
+
+})
