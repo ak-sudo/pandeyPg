beeceb8396f504a554c8ed5020eb13a4cabbe1c1
ak-sudo <72704561+ak-sudo@users.noreply.github.com>
2025-03-04 22:29:22 +0530

    Add files via upload

---
+++ store.js
@@ -0,0 +1,8 @@
+let get_item = document.querySelectorAll('.item');
+console.log(get_item);
+
+for(i=0;i<get_item.length;i++)
+    get_item[i].addEventListener('click',() => {
+        console.log(get_item.i);
+}
+)
